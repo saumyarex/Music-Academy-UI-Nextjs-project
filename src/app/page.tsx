9c7f5330fd4769a05ff36ed00f1633ab3336c95c
Saumya Nayak <saumyo224@gmail.com>
2025-06-05 01:07:00 +0530

    Instructor section component done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedCourse from "@/components/FeaturedCourse";
 import FeaturedWebinars from "@/components/FeaturedWebinars";
 import HeroSection from "@/components/HeroSection";
+import InstructorsSection from "@/components/InstructorsSection";
 import TestimonialCards from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyChooseUs />
       <TestimonialCards />
       <FeaturedWebinars />
+      <InstructorsSection />
     </main>
   );
 }
---
+++ src/components/InstructorsSection.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React from "react";
+import { AnimatedTooltip } from "./ui/animated-tooltip";
+import { BackgroundGradientAnimation } from "./ui/background-gradient-animation";
+
+const instructors = [
+  {
+    id: 1,
+    name: "Elena Briggs",
+    designation: "Vocal Coach",
+    image:
+      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 2,
+    name: "Marcus Reid",
+    designation: "Guitar Instructor",
+    image:
+      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+  },
+  {
+    id: 3,
+    name: "Julia Zhang",
+    designation: "Piano Teacher",
+    image:
+      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 4,
+    name: "Andre Gomez",
+    designation: "Drumming Expert",
+    image:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+];
+
+function InstructorsSection() {
+  return (
+    <div className="flex flex-col gap-5 justify-center items-center h-96">
+      <BackgroundGradientAnimation>
+        <div className=" mt-20 md-10 flex flex-col items-center justify-center relative z-10">
+          <h2 className="md:text-7xl text-5xl font-bold tracking-wide">
+            Meet Our Instructors
+          </h2>
+          <p className="text-lg">
+            Discover the talented professionals who will guide your musical
+            journey
+          </p>
+        </div>
+
+        <div className="flex flex-row items-center justify-center mb-10 w-full mt-5 relative z-10">
+          <AnimatedTooltip items={instructors} />
+        </div>
+      </BackgroundGradientAnimation>
+    </div>
+  );
+}
+
+export default InstructorsSection;

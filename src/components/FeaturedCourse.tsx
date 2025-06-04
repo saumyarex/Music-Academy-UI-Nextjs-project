51f470c200c47be2c7286c23891f66492bf1727c
Saumya Nayak <saumyo224@gmail.com>
2025-06-04 17:09:14 +0530

    featured card done

--- src/components/FeaturedCourse.tsx
+++ src/components/FeaturedCourse.tsx
@@ -1,7 +1,60 @@
+"use client";
 import React from "react";
+import courseData from "../data/music_courses.json";
+import { BackgroundGradient } from "./ui/background-gradient";
+import Link from "next/link";
+import { div } from "motion/react-client";
+
+interface Courses {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  price: number;
+  instructor: string;
+  isFeatured: boolean;
+  image: string;
+}
+
+const featuredCourses = courseData.courses.filter(
+  (course: Courses) => course.isFeatured === true
+);
 
 function FeaturedCourse() {
-  return <div>FeaturedCourse</div>;
+  return (
+    <div className=" flex py-10 flex-col justify-center items-center bg-gray-900">
+      <div className="text-center">
+        <h2 className="text-xl text-teal-600 font-medium">FEATURED COURSES</h2>
+        <p className="md:text-5xl text-3xl font-extrabold tracking-tight mt-3">
+          Learn With the Best
+        </p>
+      </div>
+
+      {/* Featured Courses */}
+      <div className="my-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        {featuredCourses.map((course) => (
+          <div key={course.id} className="flex mx-5">
+            <BackgroundGradient className="flex flex-col rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900 h-full">
+              <div className="flex flex-col justify-center items-center flex-grow ">
+                <h3 className="font-bold text-center">{course.title}</h3>
+                <p className="text-center mt-3">{course.description}</p>
+                <Link href={`/courses/${course.slug}`}>
+                  <button className="mt-5 text-sm sm:text-base bg-neutral-200 py-1 px-3 rounded-xs hover:cursor-pointer text-black">
+                    Learn More
+                  </button>
+                </Link>
+              </div>
+            </BackgroundGradient>
+          </div>
+        ))}
+      </div>
+      <div>
+        <button className="bg-neutral-100 hover:cursor-pointer text-black py-1 px-3 rounded-xs">
+          View All Courses
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default FeaturedCourse;

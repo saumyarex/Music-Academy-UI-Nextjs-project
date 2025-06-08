a8459ce34440c6b49992ef64cc907c82aaa906fb
Saumya Nayak <saumyo224@gmail.com>
2025-06-08 17:53:31 +0530

    Readme updated

--- README.md
+++ README.md
@@ -1,6 +1,52 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# 🎵 Music Academy UI – Built with Next.js & Aceternity UI
 
-## Getting Started
+## Description
+
+This is a Next.js project with primary focused on the UI elemtent. This project is built using UI library [Aceternity UI](https://ui.aceternity.com).
+
+## Screenshots
+
+Hero Section
+![Hero Section](./screenshots/hero-section.png)
+
+Featured Courses
+![Featured Courses](./screenshots/featured-courses.png)
+
+Sticky Scroll
+![Sticky Scroll](./screenshots/sticky-scroll.png)
+
+Testimonial Section
+![Testimonial Section](./screenshots/testimonials.png)
+
+Instructor Section and Footer
+![Instructor Section and Footer](./screenshots/footer.png)
+
+Courses Page
+![Courses Page](./screenshots/courses.png)
+
+Contact Page
+![Contact Page](./screenshots/contact.png)
+
+## 🎥 Live Demo
+
+[![Watch the demo](./screenshots/hero-section.png)](https://youtu.be/WZINW9YLxIw)
+
+## ✨ Features
+
+- Responsive hero section
+- Sticky scroll effect for course sections
+- Testimonial carousel
+- Clean and modern UI using Aceternity UI
+- Responsive and mobile-friendly layout
+
+## 🛠️ Built With
+
+- Next.js
+- React.js
+- Tailwind CSS
+- Aceternity UI
+
+## 🚀 Getting Started
 
 First, run the development server:
 
@@ -20,17 +66,6 @@ You can start editing the page by modifying `app/page.tsx`. The page auto-update
 
 This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
 
-## Learn More
+## 🙋‍♂️ Author
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+- Saumya Nayak – [@saumyarex](https://github.com/saumyarex)
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,64 +1,71 @@
 import React from "react";
 import Link from "next/link";
+
 function Footer() {
+  const date = new Date();
   return (
-    <footer className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4  font-bold sm:mx-20 mx-5 gap-10 my-10 text-lg relative z-40">
-      <div>
-        <h2>About Us</h2>
-        <p className="font-normal text-gray-400 text-base mt-4">
-          Music School is a premier institution dedicated to teaching the art
-          and science of music. We nurture talent from the ground up, fostering
-          a vibrant community of musicians.
-        </p>
+    <footer>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4  font-bold sm:mx-20 mx-5 gap-10 my-10 text-lg relative z-40">
+        <div>
+          <h2>About Us</h2>
+          <p className="font-normal text-gray-400 text-base mt-4">
+            Music School is a premier institution dedicated to teaching the art
+            and science of music. We nurture talent from the ground up,
+            fostering a vibrant community of musicians.
+          </p>
+        </div>
+        <div>
+          <h2>Quick Links</h2>
+          <ul className="font-normal text-gray-400 text-base mt-4">
+            <li className="hover:text-white hover:cursor-pointer">
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li className="hover:text-white hover:cursor-pointer">
+              <Link href={"/about"}>About</Link>
+            </li>
+            <li className="hover:text-white hover:cursor-pointer">
+              <Link href={"/courses"}>Courses</Link>
+            </li>
+            <li className="hover:text-white hover:cursor-pointer">
+              <Link href={"/contact"}>Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>Follow Us</h2>
+          <ul className="font-normal text-gray-400 text-base mt-4">
+            <li className="hover:text-white">
+              <Link href={"https://x.com/Saumya_rex"}>Twitter</Link>
+            </li>
+            <li className="hover:text-white">
+              <Link href={"https://www.instagram.com/saumya_rex/"}>
+                Instagram
+              </Link>
+            </li>
+            <li className="hover:text-white">
+              <Link href={"https://www.linkedin.com/in/saumya-a0913b171/"}>
+                LinkedIn
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2>Contact Us</h2>
+          <p className="font-normal text-gray-400 text-base mt-4">
+            New Delhi, India
+          </p>
+          <p className="font-normal text-gray-400 text-base">Delhi 10001</p>
+          <p className="font-normal text-gray-400 text-base">
+            Email: info@musicschool.com
+          </p>
+          <p className="font-normal text-gray-400 text-base">
+            Phone: (123) 456-7890
+          </p>
+        </div>
       </div>
-      <div>
-        <h2>Quick Links</h2>
-        <ul className="font-normal text-gray-400 text-base mt-4">
-          <li className="hover:text-white hover:cursor-pointer">
-            <Link href={"/"}>Home</Link>
-          </li>
-          <li className="hover:text-white hover:cursor-pointer">
-            <Link href={"/about"}>About</Link>
-          </li>
-          <li className="hover:text-white hover:cursor-pointer">
-            <Link href={"/courses"}>Courses</Link>
-          </li>
-          <li className="hover:text-white hover:cursor-pointer">
-            <Link href={"/contact"}>Contact</Link>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h2>Follow Us</h2>
-        <ul className="font-normal text-gray-400 text-base mt-4">
-          <li className="hover:text-white">
-            <Link href={"https://x.com/Saumya_rex"}>Twitter</Link>
-          </li>
-          <li className="hover:text-white">
-            <Link href={"https://www.instagram.com/saumya_rex/"}>
-              Instagram
-            </Link>
-          </li>
-          <li className="hover:text-white">
-            <Link href={"https://www.linkedin.com/in/saumya-a0913b171/"}>
-              LinkedIn
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h2>Contact Us</h2>
-        <p className="font-normal text-gray-400 text-base mt-4">
-          New Delhi, India
-        </p>
-        <p className="font-normal text-gray-400 text-base">Delhi 10001</p>
-        <p className="font-normal text-gray-400 text-base">
-          Email: info@musicschool.com
-        </p>
-        <p className="font-normal text-gray-400 text-base">
-          Phone: (123) 456-7890
-        </p>
-      </div>
+      <p className="font-normal w-full text-sm text-center text-gray-500 py-10">
+        &copy;{` ${date.getFullYear()} Music School. All rights reserved`}
+      </p>
     </footer>
   );
 }
